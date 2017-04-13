a2c7fbea810079f35400dffff0162bbdf9bcc089
Ryan Schlesinger <ryan@ryanschlesinger.com>
2017-04-13 13:26:23 -0700

    Make tire indexing jobs unique

--- lib/tire_async_index/workers/sidekiq.rb
+++ lib/tire_async_index/workers/sidekiq.rb
@@ -2,7 +2,7 @@ module TireAsyncIndex
   module Workers
     class Sidekiq < UpdateIndex
       include ::Sidekiq::Worker
-      sidekiq_options queue: TireAsyncIndex.queue
+      sidekiq_options queue: TireAsyncIndex.queue, unique_for: 30.minutes
 
       def self.enqueue(action_type, class_name, id, opts = {})
         self.perform_async(
