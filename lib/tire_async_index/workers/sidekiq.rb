9263224f52ca4275e76b89a83d784fab315bb245
Ryan Schlesinger <ryan@ryanschlesinger.com>
2017-10-02 19:36:54 -0700

    Steer activities into their own queue

--- lib/tire_async_index/workers/sidekiq.rb
+++ lib/tire_async_index/workers/sidekiq.rb
@@ -5,9 +5,15 @@ module TireAsyncIndex
       sidekiq_options queue: TireAsyncIndex.queue, unique_for: 30.minutes, unique_until: :start
 
       def self.enqueue(action_type, class_name, id, opts = {})
-        self.perform_async(
-          action_type, class_name, id, opts
-        )
+        if class_name == 'Activity'
+          self.set(queue: 'elasticsearch_activity').perform_async(
+            action_type, class_name, id, opts
+          )
+        else
+          self.perform_async(
+            action_type, class_name, id, opts
+          )
+        end
       end
 
       def perform(action_type, class_name, id, opts = {})
